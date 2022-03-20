4bdac39a11315003171f173fc1f788a46de15d17
Qara <qarahumbtov@gmail.com>
2022-03-20 23:23:19 +0400

    Add Props (isRequired, oneOfType)

--- 2. Components, Props/hello-react/src/App.js
+++ 2. Components, Props/hello-react/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import User from './components/User';
 
+// const friends = ['Nurlan', 'Ulvi', 'Tabriz', 'Fuad'];
+const friends = [
+  {
+    id: 1,
+    name: "Nurlan"
+  },
+  {
+    id: 2,
+    name: "Ulvi"
+  },
+  {
+    id: 3,
+    name: "Tabriz"
+  },
+  {
+    id: 4,
+    name: "Fuad"
+  },
+];
+
 function App() {
   return (
     <>
-      <User name="Qara" surname="Humbatov" age={22} isLoggedIn={true}/>
+      <User 
+        name="Qara" 
+        surname="Humbatov" 
+        age={22} 
+        isLoggedIn={true}
+        // friends = {friends}
+      />
     </>
   );
 }
--- 2. Components, Props/hello-react/src/components/User.js
+++ 2. Components, Props/hello-react/src/components/User.js
@@ -1,13 +1,39 @@
-function User({name, surname, age, isLoggedIn}) {
+import PropTypes from "prop-types";
+
+function User({name, surname, age, isLoggedIn, friends}) {
     return (
-        <h1> 
+        <>
+            <h1> 
+                {
+                    isLoggedIn 
+                        ? `Salam, ${surname} ${name} : ${age}` 
+                        : "Giriş edilmədi"
+                } 
+            </h1>
+
+            {/* {
+                friends.map((friend) => {
+                    const x = friend.id + 2;
+
+                    return <div key={friend.id}>{friend.name}</div>;
+                })
+            } */}
+
             {
-                isLoggedIn 
-                    ? `Salam, ${surname} ${name} : ${age}` 
-                    : "Giriş edilmədi"
-            } 
-        </h1>
+                friends && friends.map((friend) => (
+                    <div key={friend.id}>{friend.name}</div>
+                ))
+            }
+        </>
     )
+}
+
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    friends: PropTypes.array
 }
 
 export default User;
