c7cf213b7a9b9ae9d253d05bd429a014a88581bd
Qara <qarahumbtov@gmail.com>
2022-05-26 14:56:34 +0400

    İnstall Formik and basic used

--- states/src/App.js
+++ states/src/App.js
@@ -1,13 +1,18 @@
-import {useState} from "react";
+// import { useState } from "react";
+import { Formik, Field, Form } from 'formik';
 
 function App() {
+  /*
   const [name, setName] = useState("React");
   const [age, setAge] = useState(21);
   const [friends, setFriends] = useState(['Nurlan', 'Ulvi']);
   const [address, setAddress] = useState({ title: "Baku", zip: 12345 });
+  */
+
 
   return (
     <div className="App">
+      {/*   
       <h1>Hello, {name}!</h1>
       <h2>{age}</h2>
 
@@ -29,8 +34,43 @@ function App() {
       <div>{address.title} {address.zip}</div>
 
       <button onClick={() => setAddress({ ...address, title: "Gandja" })}>Change the address</button>
+    */}
+
+      <div>
+        <h1>Sign Up</h1>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+          }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <Field id="firstName" name="firstName" placeholder="Jane" />
+
+            <label htmlFor="lastName">Last Name</label>
+            <Field id="lastName" name="lastName" placeholder="Doe" />
+
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
     </div>
+
   );
+
 }
 
 export default App;
