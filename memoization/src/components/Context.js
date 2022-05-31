48c29de25c48f025469aa9eb945157f5bd77991e
Qara <qarahumbtov@gmail.com>
2022-05-31 15:49:45 +0400

    Create Context

--- memoization/src/components/Context.js
+++ memoization/src/components/Context.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../App.css'
+import ThemeContext from '../context/ThemeContext'
+import Button from './Button'
 
 function Context() {
-  return (
-    <div>Context</div>
-  )
+    return (
+        <div className='App'>
+            <ThemeContext.Provider value="dark">
+                <Button />
+            </ThemeContext.Provider>
+        </div>
+    )
 }
 
 export default Context
